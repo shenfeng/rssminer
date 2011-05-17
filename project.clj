57e700dae26687732d725d39cb5b6dcd3dc52eba
feng <feng@onyx-international.com>
2011-05-17 08:45:35 +0800

    include deps, update gitignore

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject clj-rss "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
+                 [compojure "0.6.3"]
+                 [enlive "1.0.0"]
+                 [postgresql/postgresql "9.0-801.jdbc4"]
+                 [commons-dbcp/commons-dbcp "1.4"]
+                 [ring/ring-core "0.3.8"]
+                 [ring/ring-jetty-adapter "0.3.8"]
                  [rome/rome "0.9"]])
-                 ;; [org.clojure.contrib/trace "1.3.0-alpha4"]
-                 ;; [org.clojure.contrib/lazy-xml "1.3.0-alpha4"]])
