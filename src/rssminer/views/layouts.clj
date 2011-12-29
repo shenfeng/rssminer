8236c5d6f4949e0141dbd7ccf4921c2bec8e90ca
feng <feng.shen@onycloud.com>
2011-12-29 21:18:39 +0800

    simplify macro define

--- src/rssminer/views/layouts.clj
+++ src/rssminer/views/layouts.clj
@@ -1,27 +1,21 @@
 (ns rssminer.views.layouts
-  (:require net.cgrand.enlive-html
-            rssminer.config
+  (:require [net.cgrand.enlive-html :as html]
+            [rssminer.config :as conf]
             [clojure.string :as str]))
 
 (def ^{:private true} profile-specific
-  '([(net.cgrand.enlive-html/attr= :data-profile "dev")]
-      (if (rssminer.config/in-dev?) identity
-          (net.cgrand.enlive-html/substitute ""))
-      [(net.cgrand.enlive-html/attr= :data-profile "prod")]
-        (if (rssminer.config/in-prod?) identity
-            (net.cgrand.enlive-html/substitute ""))))
+  `([(html/attr= :data-profile "dev")]
+      (if (conf/in-dev?) identity (html/substitute ""))
+      [(html/attr= :data-profile "prod")]
+        (if (conf/in-prod?) identity (html/substitute ""))))
 
 (defmacro snippet [source selector args & forms]
   (let [with-profile (concat profile-specific forms)]
-    `(net.cgrand.enlive-html/snippet ~source ~selector ~args ~@with-profile)))
+    `(html/snippet ~source ~selector ~args ~@with-profile)))
 
 (defmacro template [source args & forms]
   (let [with-profile (concat profile-specific forms)]
-    `(comp (fn [b#]
-             {:body (filter (complement str/blank?) b#)
-              :headers {"Content-Type" "text/html; charset=utf-8"}})
-           (net.cgrand.enlive-html/template
-            ~source ~args ~@with-profile))))
+    `(html/template ~source ~args ~@with-profile)))
 
 (defmacro deftemplate [name source args & forms]
   `(def ~name (template ~source ~args ~@forms)))
@@ -30,4 +24,4 @@
   `(def ~name (snippet ~source ~selector ~args ~@forms)))
 
 (deftemplate layout "templates/layout.html" [body]
-  [:#main] (net.cgrand.enlive-html/substitute body))
+  [:#main] (html/substitute body))
