db0813f3a741350cecdcba77fe9918522e46881d
feng <feng.shen@onycloud.com>
2012-05-24 20:21:41 +0800

    focus input when loaded

--- public/js/rssminer/landing.js
+++ public/js/rssminer/landing.js
@@ -1,9 +1,18 @@
 (function () {
   $('#slides').slides({
     preload: false,
-    preloadImage: '/imgs/loading.gif',
     play: 5000,
     pause: 2500,
     hoverPause: true
   });
+
+  setTimeout(function () {
+    var $email = $('#email'),
+        $pwd = $('#password');
+    if($.trim($email.val())) {
+      $pwd.focus();
+    } else {
+      $email.focus();
+    }                           // wait for browser fill form
+  }, 800);
 })();
