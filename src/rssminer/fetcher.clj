475a8d15e3c68f830e2a5f190e303f67803e68d5
feng <feng.shen@onycloud.com>
2012-06-22 11:19:36 +0800

    robust fetcher thread; not die when mysql restart

--- src/rssminer/fetcher.clj
+++ src/rssminer/fetcher.clj
@@ -1,5 +1,5 @@
 (ns rssminer.fetcher
-  (:use [clojure.tools.logging :only [trace info]]
+  (:use [clojure.tools.logging :only [trace info error]]
         (rssminer [util :only [assoc-if now-seconds]]
                   [parser :only [parse-feed]]
                   [redis :only [fetcher-dequeue fetcher-enqueue]]
@@ -59,16 +59,19 @@
     (getProxy [this] (:proxy @rssminer-conf))
     (onThrowable [this t]
       (info (str "id:" (:id link)) url (.getMessage ^Throwable t))
-      (db/update-rss-link (:id link)
-                          (let [interval (slower (:check_interval link))]
-                            {:check_interval interval
-                             :next_check_ts (+ (now-seconds) interval)
-                             :error_msg (.getMessage ^Throwable t)})))
+      (try
+        (db/update-rss-link (:id link)
+                            (let [interval (slower (:check_interval link))]
+                              {:check_interval interval
+                               :next_check_ts (+ (now-seconds) interval)
+                               :error_msg (.getMessage ^Throwable t)}))
+        (catch Exception e (error e url))))
     (getHeaders [this]
       {HttpUtils/IF_MODIFIED_SINCE last_modified
        HttpUtils/IF_NONE_MATCH etag})
     (doTask [this status headers body]
-      (handle-resp link status headers body))
+      (try (handle-resp link status headers body)
+           (catch Exception e (error e url))))
     (toString [this]
       (str (.getUri this) " " (.getHeaders this)))))
 
