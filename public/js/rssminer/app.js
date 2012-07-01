6356aff08b4fb493dd4d1d68f00887d6f3b11517
feng <feng.shen@onycloud.com>
2012-07-01 21:26:03 +0800

    encourage try again if import error

--- public/js/rssminer/app.js
+++ public/js/rssminer/app.js
@@ -235,12 +235,13 @@
 
   function show_server_message () { // only show once
     if(_RM_.gw) {               // google import wait
-      var msg = 'Busy importing from google reader, refresh in a few seconds';
+      var msg = 'Busy importing from google reader, please refresh in a few seconds';
       notify.show_msg(msg, 10000);
       _RM_.gw = 0;
     }
     if(_RM_.ge) {
-      notify.show_msg('Error import from google reader : ' + _RM_.ge, 4000);
+      var msg2 = 'Error: ' + _RM_.ge + ' , please try again';
+      notify.show_msg(msg2, 4000);
       _RM_.ge = 0;
     }
   }
