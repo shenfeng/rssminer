f10deeafaaf7c1ae7afce9c0638a473f8db930eb
feng <feng.shen@onycloud.com>
2012-08-09 19:49:21 +0800

    better code for keyboard shortcut

--- public/js/rssminer/keyboard.js
+++ public/js/rssminer/keyboard.js
@@ -4,6 +4,7 @@
       notify = window.RM.notify;
 
   var $logo = $('#logo'),
+      $q = $('#q'),
       $feed_content = $('#feed-content'),
       $reading_area = $('#reading-area'),
       $subs_list = $('#sub-list'),    // sub list
@@ -16,13 +17,62 @@
     return $reading_area.hasClass(SHOW_CONTENT);
   }
 
-  var prev_key = 0;
+  function list_keyboard (e) {
+    var $all = $subs_list.find('.folder, .item'),
+        $selected = $all.filter('.selected')[0],
+        idx = $.inArray($selected, $all),
+        href,
+        pre = 0,
+        next = 0;
+    if(idx !== -1) {
+      pre = idx === 0 ? 0 : idx - 1;
+      next = idx === $all.length -1 ? idx : idx + 1;
+    } else {
+      pre = 0;
+      next = 0;
+    }
+    switch(e.which) {
+    case 74:                  // j
+      var $n = $($all[next]);
+      href = $n.find('a').attr('href');
+      if(!href) {             // a, folder
+        href = $n.attr('href');
+      }
+      location.hash = href;
+      break;
+    case 75:                  // k
+      var $p = $($all[pre]);
+      href = $p.find('a').attr('href');
+      if(!href) {             // a, folder
+        href = $p.attr('href');
+      }
+      location.hash = href;
+      break;
+    case 79:
+      var $f = $welcome_list.find('.feed:first').find('a');
+      href = $f.attr('href');
+      location.hash = href;
+      break;
+    case 84:                  // t, swith tab
+      var $tabs = $('.sort li');
+      var selected = $tabs.filter('.selected')[0];
+      var s = $.inArray(selected, $tabs);
+      if(s === $tabs.length - 1) {
+        s = 0;
+      } else {
+        s += 1;
+      }
+      location.hash = $($tabs[s]).find('a').attr('href');
+      break;
+    }
+  }
 
   function keyboard_shortcut (e) {
     if(e.which === 191) {       // /
-      $('#q').click();
+      $q.focus().click();
       return false;
     }
+    if($q.is(':focus')) { return; }
     if(is_reading()) {
       var $feed_list = $('#feed-list'),
           $current = $('#feed-list .selected'),
@@ -31,7 +81,6 @@
       switch(e.which) {
       case 85:
         var args = /#read\/(.+)\/\d+\?(.+?)s=(.+)/.exec(location.hash);
-        // console.log(args);
         if(/^\d+$/.test(args[1]) || /f_.*/.test(args[1])) {
           location.hash = 'read/' + args[1] + '?p=1&s=' + args[3];
         } else {
@@ -55,50 +104,8 @@
         break;
       }
     } else {
-      var $all = $subs_list.find('.folder, .item');
-      var $selected = $all.filter('.selected')[0];
-      var idx = $.inArray($selected, $all);
-      var pre = 0, next = 0;
-      if(idx !== -1) {
-        pre = idx === 0 ? 0 : idx - 1;
-        next = idx === $all.length -1 ? idx : idx + 1;
-      } else { pre = 0; next = 0; }
-      switch(e.which) {
-      case 74:                  // j
-        var $n = $($all[next]);
-        var href = $n.find('a').attr('href');
-        if(!href) {             // a, folder
-          href = $n.attr('href');
-        }
-        location.hash = href;
-        break;
-      case 75:                  // k
-        var $p = $($all[pre]);
-        var href = $p.find('a').attr('href');
-        if(!href) {             // a, folder
-          href = $p.attr('href');
-        }
-        location.hash = href;
-        break;
-      case 79:
-        var $f = $welcome_list.find('.feed:first').find('a');
-        var href = $f.attr('href');
-        location.hash = href;
-        break;
-      case 84:                  // t, swith tab
-        var $tabs = $('.sort li');
-        var selected = $tabs.filter('.selected')[0];
-        var idx = $.inArray(selected, $tabs);
-        if(idx === $tabs.length - 1) {
-          idx = 0;
-        } else {
-          idx += 1;
-        }
-        location.hash = $($tabs[idx]).find('a').attr('href');
-        break;
-      }
+      return list_keyboard(e);
     }
-    prev_key = e.which;
   }
 
   $(document).keydown(keyboard_shortcut);
