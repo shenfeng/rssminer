0cc54de2aaa640eb70b66e0a7a6692e56b17e2e3
feng <feng.shen@onycloud.com>
2012-08-29 21:52:23 +0800

    more term for test search perf

--- test/scripts/req_test.sh
+++ test/scripts/req_test.sh
@@ -1,13 +1,17 @@
 #! /bin/bash
 
-ID=
+set -u                          # Treat unset variables as an error
+set -e #  Exit immediately if a simple command exits with a non-zero status
+
+# SET it
+# ID=
 PORT=9090
 
 TERMS=(
     'java' 'ios' "clojure" "谷歌四面树敌" "史诗般的战争"
-    "debian" "做正确的加法" "编程" "网页中的平面构成"
-    "2从原理分析PHP性能" "nginx" "排名算法" "电子商务"
-    "创新工场" "手机" "周鸿祎"
+    "debian" "做正确的加法" "编程" "网页中的平面构成" "企业开发者" "Nosql" "google"
+    "2从原理分析PHP性能" "nginx" "排名算法" "电子商务" "software"
+    "创新工场" "手机" "周鸿祎" "sql" "企业应用测试平台" "融资" 
 )
 COUNT=${#TERMS[@]}
 
@@ -26,7 +30,7 @@ URLS=(
 URL_COUNT=${#URLS[@]}
 
 function ab_test() {
-    QPS=$(ab -n 3000 -c 20 -C _id_=$ID $1 2>&1 | grep "Requests")
+    QPS=$(ab -n 5000 -c 50 -C _id_=$ID $1 2>&1 | grep "Requests")
     echo -e $1 ":\t"  $QPS
 }
 
@@ -34,15 +38,8 @@ for i in {1..200}; do
     IDX=$(expr $i % $COUNT)
     TERM=${TERMS[$IDX]}
     ab_test "http://127.0.0.1:$PORT/api/search?q=${TERM}&limit=11"
-
-    if [ $IDX -eq 6 ]; then
-        echo "recompute all"
-        wget --header "Cookie:_id_=$ID" "http://127.0.0.1:$PORT/admin/compute" -O /dev/null
-    fi
-
     IDX=$(expr $i % $URL_COUNT)
     URL=${URLS[$IDX]}
     ab_test $URL
-
     sleep 1
 done
