51da40d1e64a2d1004e1661729fd0f29c26ca264
feng <feng.shen@onycloud.com>
2012-10-10 18:15:21 +0800

    calculate hammingDistance concurrently

--- test/java/rssminer/NearDuplicate.java
+++ test/java/rssminer/NearDuplicate.java
@@ -6,6 +6,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import me.shenfeng.dbcp.PerThreadDataSource;
 
@@ -15,6 +18,54 @@ import org.slf4j.LoggerFactory;
 import rssminer.search.Searcher;
 import rssminer.tools.Utils;
 import clojure.lang.Keyword;
+
+class Result {
+    final int id;
+    final long[] counter;
+
+    public Result(int id, long[] counter) {
+        this.id = id;
+        this.counter = counter;
+    }
+}
+
+class Worker extends Thread {
+    // private static final Logger logger =
+    // LoggerFactory.getLogger(Worker.class);
+
+    private long[] feedhashes;
+    private AtomicInteger id;
+    private BlockingQueue<Result> done;
+
+    public Worker(long[] feedhashes, AtomicInteger id,
+            BlockingQueue<Result> done) {
+        this.feedhashes = feedhashes;
+        this.id = id;
+        this.done = done;
+    }
+
+    public void run() {
+        int i = 0;
+        while ((i = id.incrementAndGet()) < feedhashes.length) {
+            long me = feedhashes[i];
+            // logger.info("handle {}, me: {}", i, me);
+            if (me != -1) {
+                long innerCounter[] = new long[65];
+                for (int j = i + 1; j < feedhashes.length; j++) {
+                    long other = feedhashes[j];
+                    if (other != -1) {
+                        int d = SimHash.hammingDistance(me, other);
+                        innerCounter[d] += 1;
+                    }
+                }
+                try {
+                    done.put(new Result(i, innerCounter));
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
 
 public class NearDuplicate implements Runnable {
 
@@ -50,7 +101,8 @@ public class NearDuplicate implements Runnable {
         return result;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException,
+            InterruptedException {
         Map<Keyword, Object> config = new HashMap<Keyword, Object>();
         PerThreadDataSource db = new PerThreadDataSource(
                 "jdbc:mysql://localhost/rssminer", "feng", "");
@@ -60,37 +112,35 @@ public class NearDuplicate implements Runnable {
         NearDuplicate duplicate = new NearDuplicate();
         duplicate.run();
         logger.info("all computed");
+
+        AtomicInteger id = new AtomicInteger(0);
+        BlockingQueue<Result> done = new ArrayBlockingQueue<Result>(100);
+
+        int processors = Runtime.getRuntime().availableProcessors();
+        for (int i = 0; i <= processors; i++) {
+            Worker w = new Worker(feedhashes, id, done);
+            w.setName("worker-" + i);
+            w.setDaemon(true);
+            w.start();
+        }
         int counter[] = new int[65];
-        for (int i = 0; i < feedhashes.length; i++) {
-            long me = feedhashes[i];
-            if (me != -1) {
-                long innerCounter[] = new long[65];
-                for (int j = i + 1; j < feedhashes.length; j++) {
-                    long other = feedhashes[j];
-                    if (other != -1) {
-                        int d = SimHash.hammingDistance(me, other);
-                        // if (d == 0) {
-                        // logger.info("{}:{}, {}:{}", new Object[] { i, me,
-                        // j, other });
-                        // }
-                        innerCounter[d] += 1;
-                    }
-                }
-                for (int j = 0; j < innerCounter.length; j++) {
-                    counter[j] += innerCounter[j];
-                }
+        while (id.get() < feedhashes.length) {
+            Result r = done.take();
+            long innerCounter[] = r.counter;
+            for (int j = 0; j < innerCounter.length; j++) {
+                counter[j] += innerCounter[j];
             }
-            if (i % 5000 == 0) {
-                logger.info(i + ": " + Arrays.toString(counter));
+            if (r.id % 5000 == 0) {
+                logger.info(r.id + ": " + Arrays.toString(counter));
             }
         }
+        logger.info(Arrays.toString(counter));
     }
 
     public void run() {
         try {
             logger.info("init NearDuplicate");
             int max = Utils.getMaxID();
-            // max = 150000;
             long[] hashes = new long[max + 1];
 
             for (int i = 0; i <= max; i++) {
