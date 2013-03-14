9634361010b8d4ab432415d4b0a5225f340fd1d0
feng <feng.shen@onycloud.com>
2013-03-14 19:16:18 +0800

    do not show importing subscriptions list is not empty

--- public/js/rssminer/app.js
+++ public/js/rssminer/app.js
@@ -413,9 +413,11 @@
 
   function show_server_message () { // only show once
     if(_RM_.gw) {               // google import wait
-      var msg = 'Busy importing from google reader, please refresh in a few seconds';
-      notify.show_msg(msg, 10000);
-      _RM_.gw = 0;
+      if(!$('#sub-list .item').length) {
+        var msg = 'Busy importing from google reader, please refresh in a few seconds';
+        notify.show_msg(msg, 10000);
+        _RM_.gw = 0;
+      }
     }
     if(_RM_.ge) {
       var msg2 = 'Error: ' + _RM_.ge + ' , please try again';
