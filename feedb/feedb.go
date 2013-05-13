6467ae717673d389c0e019affecf73e2128950b0
feng <shenedu@gmail.com>
2013-05-13 15:20:05 +0800

    multi get => response as json

--- feedb/feedb.go
+++ feedb/feedb.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"compress/zlib"
 	"encoding/binary"
+	"encoding/json"
 	"errors"
 	"flag"
 	"hash/crc32"
@@ -20,6 +23,7 @@ var (
 	addr      = flag.String("addr", ":7167", "HTTP service address (e.g., ':7167') ")
 	NOT_FOUND = errors.New("Not found")
 	CORRUPTED = errors.New("Corrupted")
+	ZERO_KEY  = errors.New("Key can not be zero")
 )
 
 const (
@@ -30,6 +34,11 @@ const (
 	MIN_DATA_LENGtH  = 1024 * 1024 * 64
 	DATA_GROW_STEP   = 1024 * 1024 * 256
 )
+
+type Item struct {
+	Id   uint64
+	Data string
+}
 
 type Dict struct {
 	mu    sync.Mutex
@@ -43,43 +52,36 @@ type Feedb struct {
 	tables map[string]*Dict
 }
 
+func segmentPath(path string) (name string, ids string) {
+	name = path
+	idx := strings.Index(path, "/")
+	if idx > 0 {
+		name = path[:idx]
+		ids = path[idx+1:]
+	}
+	return
+}
+
 func (db *Feedb) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	uri := r.RequestURI
-	if strings.HasPrefix(uri, DB_PREFEX) {
-		partiton, exits := db.tables[r.URL.Path[3:]]
+	if strings.HasPrefix(r.RequestURI, DB_PREFEX) {
+		name, ids := segmentPath(r.URL.Path[3:])
+		partiton, exits := db.tables[name]
 		if !exits {
 			http.Error(w, "404 db not found", http.StatusNotFound)
 			return
 		}
-
 		r.ParseForm()
-		id, err := strconv.ParseUint(r.Form.Get("id"), 10, 64)
-
-		if err != nil || id <= 0 {
-			http.Error(w, "id parameter is mandatatory", 400)
-			return
-		}
-
 		switch r.Method {
 		case "GET":
-			data, err := partiton.Get(id)
-			if err != nil {
-				http.Error(w, err.Error(), 404)
-				return
-			}
-			if r.Form.Get("text") != "" {
-				w.Header().Set("Content-Type", "text/plain")
-			} else {
-				w.Header().Set("Content-Type", "application/octet-stream")
-			}
-
-			if r.Form.Get("deflate") != "" {
-				w.Header().Set("Content-Encoding", "deflate")
-			}
-			//			w.WriteHeader(200)
-			w.Write(data)
+			partiton.GetAndResponse(ids, w, r)
 			return
 		case "POST":
+			id, err := strconv.ParseUint(r.Form.Get("id"), 10, 64)
+			if err != nil || id <= 0 {
+				http.Error(w, "id parameter is mandatatory", 400)
+				return
+			}
+
 			count, _ := strconv.ParseInt(r.Form.Get("len"), 10, 64)
 			body, err := ioutil.ReadAll(r.Body)
 			if err != nil || count <= 0 || count != int64(len(body)) {
@@ -96,7 +98,69 @@ func (db *Feedb) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "404 page not found", http.StatusNotFound)
 }
 
+func deflateData(data []byte) ([]byte, error) {
+	reader, err := zlib.NewReader(bytes.NewReader(data))
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadAll(reader)
+}
+
+func (db *Dict) GetAndResponse(idstr string, w http.ResponseWriter, r *http.Request) {
+	deflate := r.Form.Get("deflate") != ""
+
+	if strings.Index(idstr, "-") != -1 {
+		ids := strings.Split(idstr, "-")
+		items := make([]Item, len(ids))
+		for idx, i := range ids {
+			id, _ := strconv.ParseUint(i, 10, 64)
+			item := Item{Id: id}
+			data, err := db.Get(id)
+			if err == nil {
+				if deflate {
+					d, err := deflateData(data)
+					if err == nil {
+						item.Data = string(d)
+					}
+				} else {
+					item.Data = string(data)
+				}
+			}
+			items[idx] = item
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		bytes, err := json.Marshal(items)
+		if err != nil {
+			w.Write([]byte(err.Error()))
+		} else {
+			w.Write(bytes)
+		}
+	} else {
+		id, _ := strconv.ParseUint(idstr, 10, 64)
+		data, err := db.Get(id)
+		if err != nil {
+			http.Error(w, err.Error(), 404)
+			return
+		}
+		if r.Form.Get("text") != "" {
+			w.Header().Set("Content-Type", "text/plain")
+		} else {
+			w.Header().Set("Content-Type", "application/octet-stream")
+		}
+
+		if deflate {
+			w.Header().Set("Content-Encoding", "deflate")
+		}
+		//			w.WriteHeader(200)
+		w.Write(data)
+	}
+}
+
 func (db *Dict) Get(key uint64) (data []byte, err error) {
+	if key <= 0 {
+		return nil, ZERO_KEY
+	}
+
 	// TODO make sure no lock is needed
 	if uint64(len(db.index)) < key*8 {
 		return nil, NOT_FOUND
@@ -116,10 +180,14 @@ func (db *Dict) Get(key uint64) (data []byte, err error) {
 		return nil, CORRUPTED
 	}
 
+	// log.Println(string(ioutil.ReadAll(flate.NewReader(bytes.NewReader(r)))))
 	return r, nil
 }
 
 func (db *Dict) Set(key int, bytes []byte) error {
+	if key <= 0 {
+		return ZERO_KEY
+	}
 	db.mu.Lock()
 	defer db.mu.Unlock()
 	indexIdx := key * 8
@@ -214,7 +282,17 @@ func readExitingFiles(dbroot string) (db *Feedb) {
 func main() {
 	flag.Parse()
 
-	log.Println(100, 100>>0)
+	// st := []byte("sdfsdf")
+
+	// a := Item{
+	// 	Id:   1,
+	// 	Data: []byte{'a'},
+	// }
+
+	// j, e := json.Marshal([]Item{a})
+	// log.Println(e, string(j))
+
+	// log.Println(100, 100>>0)
 	//	log.SetFlags(log.Llongfile | log.LstdFlags)
 
 	//	arr := []byte{1, 2 , 3, 4, 5, 6, 7, 8}
--- feedb/test.py
+++ feedb/test.py
@@ -6,7 +6,7 @@ HOST = "127.0.0.1:7167"
 
 def test_ok(con, id, data):
     # con = httplib.HTTPConnection(HOST)
-    con.request('GET', '/d/test?id=' + str(id))
+    con.request('GET', '/d/test/' + str(id))
     resp = con.getresponse()
     status = resp.status
     if status != 200:
@@ -34,9 +34,10 @@ def send_and_get(id, ids=[]):
     ids.append(id)
 
     test_ok(con, ids[random.randint(0, len(ids) - 1)], None)
+    test_ok(con, "-".join(map(str, ids)), None)
     con.close()
 
 
 if __name__ == '__main__':
-    for i in xrange(1, 500):
+    for i in xrange(1, 5):
         send_and_get(random.randint(1, 1024 * 10240))
